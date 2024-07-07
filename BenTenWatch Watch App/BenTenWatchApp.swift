b99b115da462058f21a464adab2b91f2fb0825cd
timothy <tandrian24@bsd.idserve.net>
2024-07-07 22:37:09 +0700

    Initial Commit

---
+++ BenTenWatch Watch App/BenTenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenTenWatchApp.swift
+//  BenTenWatch Watch App
+//
+//  Created by Timothy Andrian on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BenTenWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
